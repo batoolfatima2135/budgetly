8925c0aaad545c3adfda14373685e2c94b8b169b
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-17 19:09:28 +0500

    Remove uneccesary tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ApplicationRecord
   validates :name,  presence: true
   has_many :categories, foreign_key: "user_id"
   has_many :payments, foreign_key: "author_id"
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
