f25aea56b0becda83dc9d7f3e3476bf3741ab936
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-17 18:40:17 +0500

    Add more tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_16_162121) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_17_094436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_16_162121) do
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
+  create_table "payments", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "author_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_payments_on_author_id"
+    t.index ["category_id"], name: "index_payments_on_category_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "name", default: "", null: false
@@ -67,4 +78,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_16_162121) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "categories", "users"
+  add_foreign_key "payments", "categories"
+  add_foreign_key "payments", "users", column: "author_id"
 end
