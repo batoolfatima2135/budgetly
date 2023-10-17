61ead3ddc4f9fd870e886f27e861c230e83f6642
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-17 18:39:05 +0500

    Add relations and validations to models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   validates :name , :icon, presence: true
   belongs_to :user
+  has_many :payments
   has_one_attached :icon
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,5 @@
 class Payment < ApplicationRecord
-  
-  belongs_to :author
+  validates :name, :amount, presence: true
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :category
 end
