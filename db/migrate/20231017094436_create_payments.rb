fbce5a43fece7d283b4a4e2bd7332288cef4a1f4
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-17 18:39:37 +0500

    Add table for payments

--- db/migrate/20231017094436_create_payments.rb
+++ db/migrate/20231017094436_create_payments.rb
@@ -3,7 +3,7 @@ class CreatePayments < ActiveRecord::Migration[7.0]
     create_table :payments do |t|
       t.string :name
       t.decimal :amount
-      t.references :author, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: :users }
       t.belongs_to :category, null: false, foreign_key: true
 
       t.timestamps
