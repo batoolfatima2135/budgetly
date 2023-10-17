ba737e6ab8662832f24bb22ade99807f9e635d19
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-17 18:39:13 +0500

    Add relation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :name,  presence: true
-  has_many :categoriess, foreign_key: "user_id"
+  has_many :categories, foreign_key: "user_id"
+  has_many :payments, foreign_key: "author_id"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
